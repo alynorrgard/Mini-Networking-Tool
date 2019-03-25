4d99d0fea4f92f15e421f85447f9c62b20b67849
Aly Norrgard <amnorrgard@gmail.com>
2019-03-25 17:32:18 -0400

    Tier 1: added links to navbar for all-campuses/all-students views, added routes to Home, AllCampuses, AllStudents components

--- app/components/root.js
+++ app/components/root.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Home from './HomeComponent';
+import AllCampuses from './AllCampusesComponent';
+import AllStudents from './AllStudentsComponent';
 
 const Root = () => {
   return (
-    <div>
-      <nav>
-        Welcome!
-      </nav>
-      <main>
-        <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-        <p>This seems like a nice place to get started with some Routes!</p>
-      </main>
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          Welcome!
+          <Link to="/campuses">Campuses</Link>
+          <Link to="/students">Students</Link>
+        </nav>
+        <main>
+          <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
+          <p>This seems like a nice place to get started with some Routes!</p>
+        </main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/campuses" component={AllCampuses} />
+          <Route exact path="/students" component={AllStudents} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-export default Root
+export default Root;
