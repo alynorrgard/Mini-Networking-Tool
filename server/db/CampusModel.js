fde39fabccafc758c59ef543f129c3c0e5f3a5d6
Aly Norrgard <amnorrgard@gmail.com>
2019-03-25 15:32:23 -0400

    Tier 1: wrote 'Campus' model

---
+++ server/db/CampusModel.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const db = require('./index');
+
+module.exports = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://expo.advance.net/img/5cb26d37b6/width960/109_thecommonsnortheastentry.jpeg',
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+});
