90ee42c0ef50fffb15965026742470d551999e8d
Aly Norrgard <amnorrgard@gmail.com>
2019-03-26 10:33:09 -0400

    Tier 2: wrote routes to successfully serve up a single campus/student with eager loading by foreign key - Tier 2 Backend complete

--- server/api/campusRouter.js
+++ server/api/campusRouter.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { Campus } = require('../db/index');
+const { Campus, Student } = require('../db/index');
 
 router.get('/', async (req, res, next) => {
   try {
     const campuses = await Campus.findAll();
     res.send(campuses);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:campusId', async (req, res, next) => {
+  try {
+    const campusById = await Campus.findById(req.params.campusId, {
+      include: { model: Student },
+    });
+    res.send(campusById);
   } catch (err) {
     next(err);
   }
--- server/api/studentRouter.js
+++ server/api/studentRouter.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { Student } = require('../db/index');
+const { Student, Campus } = require('../db/index');
 
 router.get('/', async (req, res, next) => {
   try {
     const students = await Student.findAll();
     res.send(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:studentId', async (req, res, next) => {
+  try {
+    const studentById = await Student.findById(req.params.studentId, {
+      include: { model: Campus },
+    });
+    res.send(studentById);
   } catch (err) {
     next(err);
   }
