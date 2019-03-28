b0e8e9eb654ee21b3d8954ef8394c6866e20034a
Aly Norrgard <amnorrgard@gmail.com>
2019-03-28 11:31:11 -0400

    Tier 2: moved 'this.setState()' out of 'componentDidMount' on single campus/student files

--- app/components/SingleCampusComponent.js
+++ app/components/SingleCampusComponent.js
@@ -8,9 +8,14 @@ class SingleCampus extends React.Component {
     this.state = {
       campus: {},
     };
+    this.displayCampus = this.displayCampus.bind(this);
   }
-  // whenever state changes it will trigger a re-render:
-  async componentDidMount() {
+
+  componentDidMount() {
+    this.displayCampus();
+  }
+
+  async displayCampus() {
     try {
       const selectedCampus = await axios.get(
         `/api/campuses/${this.props.match.params.campusId}`
@@ -25,6 +30,7 @@ class SingleCampus extends React.Component {
 
   render() {
     const campus = this.state.campus;
+    console.log('SINGLE CAMPUS:', campus);
     return (
       <div className="campus-page">
         <div>
@@ -33,19 +39,17 @@ class SingleCampus extends React.Component {
           <h3>{campus.address}</h3>
           <p>{campus.description}</p>
         </div>
+        <p>AFFILIATED STUDENTS:</p>
         {campus.students ? (
-          <div>
-            <p>AFFILIATED STUDENTS:</p>
-            {campus.students.map(student => {
-              return (
-                <p className="single-page-link" key={student.id}>
-                  <Link to={`/students/${student.id}`}>
-                    {student.firstName} {student.lastName}
-                  </Link>
+          campus.students.map(student => {
+            return (
+              <Link to={`/students/${student.id}`} key={student.id}>
+                <p className="single-page-link">
+                  {student.firstName} {student.lastName}
                 </p>
-              );
-            })}
-          </div>
+              </Link>
+            );
+          })
         ) : (
           <p>This campus does not have any students yet!</p>
         )}
--- app/components/SingleStudentComponent.js
+++ app/components/SingleStudentComponent.js
@@ -8,14 +8,18 @@ class SingleStudent extends React.Component {
     this.state = {
       student: {},
     };
+    this.displayStudent = this.displayStudent.bind(this);
   }
-  // whenever state changes it will trigger a re-render:
-  async componentDidMount() {
+
+  componentDidMount() {
+    this.displayStudent();
+  }
+
+  async displayStudent() {
     try {
       const selectedStudent = await axios.get(
         `/api/students/${this.props.match.params.studentId}`
       );
-      console.log('STUDENT DATA:', selectedStudent.data);
       this.setState({
         student: selectedStudent.data,
       });
@@ -26,7 +30,6 @@ class SingleStudent extends React.Component {
 
   render() {
     const student = this.state.student;
-    console.log('STUDENT STATE:', student);
     return (
       <div className="student-page">
         <h2>
@@ -35,14 +38,12 @@ class SingleStudent extends React.Component {
         <img src={student.imageUrl} className="student-image" />
         <p>EMAIL: {student.email}</p>
         <p>GPA: {student.gpa}</p>
+        <p>CAMPUS AFFILIATION:</p>
         {student.campus ? (
           <div>
-            <p>CAMPUS AFFILIATION:</p>
-            <p className="single-page-link">
-              <Link to={`/campuses/${student.campus.id}`}>
-                {student.campus.name}
-              </Link>
-            </p>
+            <Link to={`/campuses/${student.campus.id}`}>
+              <p className="single-page-link">{student.campus.name}</p>
+            </Link>
           </div>
         ) : (
           <p>This student is not yet enrolled at a campus!</p>
